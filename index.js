85a73211d6ae5431c35ad2ee67152a817aa179ed
itsjoesullivan <itsjoesullivan@gmail.com>
2014-04-25 20:54:40 -0400

    ref Module

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-Module.TOTAL_MEMORY = 16777216 * 4;
 
 var Iago = module.exports = function() {
+  if(! window.Module ) {
+    require('iago/vorbis.small.js');
+  }
 
   this.input = context.createScriptProcessor(1024, 2, 2);
   this.input.onaudioprocess = this.write.bind(this);
 
-  this.state = _lexy_encoder_start(44100, 3);
+  this.state = Module._lexy_encoder_start(44100, 3);
 
 };
 
 
 Iago.prototype.write = function( e ) {
   if (this.done) {
+    return;
     throw "No more writes.";
   }
 
   var buffers = [ e.inputBuffer.getChannelData(0), e.inputBuffer.getChannelData(1) ];
 
   // Grab data
-  var inbuf_l = _malloc(buffers[0].length * 4);
-  var inbuf_r = _malloc(buffers[1].length * 4);
+  var inbuf_l = Module._malloc(buffers[0].length * 4);
+  var inbuf_r = Module._malloc(buffers[1].length * 4);
   for (var i=0;i<buffers[0].length;i++) {
-    setValue(inbuf_l + (i*4), buffers[0][i], 'float');
-    setValue(inbuf_r + (i*4), buffers[1][i], 'float');
+    Module.setValue(inbuf_l + (i*4), buffers[0][i], 'float');
+    Module.setValue(inbuf_r + (i*4), buffers[1][i], 'float');
   }
 
   // Write data
-  _lexy_encoder_write( this.state, inbuf_l, inbuf_r, buffers[0].length );
+  Module._lexy_encoder_write( this.state, inbuf_l, inbuf_r, buffers[0].length );
 
 };
 
@@ -39,13 +42,13 @@ Iago.prototype.getBlob = function() {
   this.done = true;
 
   // Wrap up
-  _lexy_encoder_finish( this.state );
+  Module._lexy_encoder_finish( this.state );
 
   // Get pointer
-  var ptr = _lexy_get_buffer( this.state );
+  var ptr = Module._lexy_get_buffer( this.state );
 
   var oggData = new Uint8Array( Module.HEAPU8.subarray( ptr, 
-    ptr + _lexy_get_buffer_length( this.state ) ) 
+    ptr + Module._lexy_get_buffer_length( this.state ) ) 
   );
 
   // Create blob
