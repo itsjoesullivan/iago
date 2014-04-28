6396b77ecfbb531dc13115150a9087a6c816e436
itsjoesullivan <itsjoesullivan@gmail.com>
2014-04-28 17:00:32 -0400

    works with thread

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
+var Thread = require('thread');
 
 var Iago = module.exports = function(object) {
-  if(! window.Module ) {
-    require('./vorbis.small.js');
-  }
 
-  // Handle AudioBuffers immediately?
-  if (object instanceof window.AudioBuffer) {
-    this.buffer = object;
-    this.state = Module._lexy_encoder_start(44100, 3);
-    var len = this.buffer.length;
-    var pos = 0;
-    var end = len;
-    var one = function(i) {
-      return this.buffer.getChannelData(i).subarray(pos, end);
-    }.bind(this);
-    while(len > pos) {
-      end = pos + 4096;
-      if(end > len) {
-        end = len;
-      }
+  this.thread = new Thread(function() {
 
-      this.write({
-        inputBuffer: {
-          getChannelData: one
-        }
-      });
-      pos = end;
-    }
-    this.getBlob();
-    return;
-  }
+    // Import vorbis into worker
+    importScripts( 'http://local.scat.io:3000/vorbis.small.js' );
+
+    // Initialize
+    var state = Module._lexy_encoder_start(44100, 3);
+
+    // On writes, write to vorbis
+    thread.on('write', function(data) {
+
+      var l = data[0];
+      var r = data[1];
+
+      var inbuf_l = Module._malloc(l.length*l.BYTES_PER_ELEMENT);
+      Module.HEAPF32.set( l, inbuf_l>>2 );
+
+      var inbuf_r = Module._malloc(r.length*r.BYTES_PER_ELEMENT);
+      Module.HEAPF32.set( r, inbuf_r>>2 );
+
+      Module._lexy_encoder_write( state, inbuf_l, inbuf_r, l.length );
+
+      Module._free(inbuf_l);
+      Module._free(inbuf_r);
+
+    });
+
+    thread.on('finish', function(e) {
+
+      // Wrap up
+      Module._lexy_encoder_finish( state );
+
+      // Get pointer
+      var ptr = Module._lexy_get_buffer( state );
+
+
+      var oggData = new Uint8Array( Module.HEAPU8.subarray( ptr, 
+        ptr + Module._lexy_get_buffer_length( state ) ) 
+      );
+      thread.send('blob', oggData);
+
+    });
+  });
   
-  if (object instanceof window.OfflineAudioContext) {
-    this.context = object;
-  } else if (object instanceof window.AudioContext) {
-    this.context = object;
-  } else if (object instanceof window.MediaStreamAudioSourceNode) {
+  if (object instanceof window.MediaStreamAudioSourceNode) {
     this.source = object;
     this.context = this.source.context;
   }
@@ -43,8 +54,6 @@ var Iago = module.exports = function(object) {
   this.input = this.context.createScriptProcessor(4096, 2, 0);
   this.input.onaudioprocess = this.write.bind(this);
   this.input.connect(this.context.destination);
-
-  this.state = Module._lexy_encoder_start(44100, 1);
 
 };
 
@@ -56,44 +65,29 @@ Iago.prototype.write = function( e ) {
     return;
   }
 
-  var l = e.inputBuffer.getChannelData(0);
-  var r = e.inputBuffer.getChannelData(1);
+  this.thread.send('write', 
+    e.inputBuffer.getChannelData(0),
+    e.inputBuffer.getChannelData(1) );
 
-  var inbuf_l = Module._malloc(l.length*l.BYTES_PER_ELEMENT);
-  Module.HEAPF32.set( l, inbuf_l>>2 );
-
-  var inbuf_r = Module._malloc(r.length*r.BYTES_PER_ELEMENT);
-  Module.HEAPF32.set( r, inbuf_r>>2 );
-
-  Module._lexy_encoder_write( this.state, inbuf_l, inbuf_r, l.length );
-
-  Module._free(inbuf_l);
-  Module._free(inbuf_r);
 };
 
 Iago.prototype.getBlob = function(cb) {
 
   if (this.blob) {
-    return this.blob;
+    return cb( null, this.blob );
   }
 
   this.done = true;
 
-  // Wrap up
-  Module._lexy_encoder_finish( this.state );
+  this.thread.on('blob', function( data ) {
+    this.blob = new Blob( [ data ], {
+      type: 'audio/ogg'
+    });
+    cb( null, this.blob );
+  }.bind(this));
 
-  // Get pointer
-  var ptr = Module._lexy_get_buffer( this.state );
+  this.thread.send('finish');
 
-  var oggData = new Uint8Array( Module.HEAPU8.subarray( ptr, 
-    ptr + Module._lexy_get_buffer_length( this.state ) ) 
-  );
-
-  // Create blob
-  this.blob = new Blob( [ oggData ], {
-    type: 'audio/ogg'
-  });
-  return this.blob;
 };
 
 Iago.prototype.download = function() {
